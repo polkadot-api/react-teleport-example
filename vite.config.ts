4d880b55b41d5b57236031c2653ea3dac740be55
Carlo Sala <carlosalag@protonmail.com>
2025-04-24 18:43:36 +0200

    fix: migrate to tailwind 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import vite from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vite()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
