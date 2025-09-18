fa88062522edc7f6c5cc66b8062cfe5e4e7d2acd
aigents999 <aigents999@gmail.com>
2025-09-18 10:29:31 +0200

    Descriptor error fix

--- whitelist.ts
+++ whitelist.ts
@@ -1,6 +1,8 @@
 import {
   DotAhWhitelistEntry,
   DotWhitelistEntry,
+  ItkWhitelistEntry,
+  ItpWhitelistEntry
 } from "@polkadot-api/descriptors"
 
 const dotWhitelist: DotWhitelistEntry[] = [
@@ -9,10 +11,33 @@ const dotWhitelist: DotWhitelistEntry[] = [
   "const.Balances.ExistentialDeposit",
 ]
 
+// Porteer module descriptors for Integritee Polkadot
+const itpWhitelist: ItpWhitelistEntry[] = [
+  "tx.Porteer.port_tokens",
+  "query.Porteer.LastHeartBeat",
+  "query.Porteer.PorteerConfigValue", 
+  "query.Porteer.XcmFeeConfig",
+  "event.Porteer.MintedPortedTokens",
+  "event.Porteer.ForwardedPortedTokens",
+  "event.Porteer.PortedTokens",
+]
+
+// Porteer module descriptors for Integritee Kusama
+const itkWhitelist: ItkWhitelistEntry[] = [
+  "tx.Porteer.port_tokens",
+  "query.Porteer.LastHeartBeat",
+  "query.Porteer.PorteerConfigValue", 
+  "query.Porteer.XcmFeeConfig",
+  "event.Porteer.MintedPortedTokens",
+  "event.Porteer.ForwardedPortedTokens",
+  "event.Porteer.PortedTokens",
+]
+
 const ahWhitelist: DotAhWhitelistEntry[] = [
   "tx.PolkadotXcm.transfer_assets",
   "query.ForeignAssets.Account",
   "const.Balances.ExistentialDeposit",
+  "event.ForeignAssets.Issued",
 ]
 
-export const whitelist = [...dotWhitelist, ...ahWhitelist]
+export const whitelist = [...dotWhitelist, ...ahWhitelist, ...itpWhitelist, ...itkWhitelist]
